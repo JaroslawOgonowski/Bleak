563a6c99d86f43707cf014acd56bab6f08efa80d
Jaroslaw Ogonowski <ipro.jaroslaw.ogonowski@gmail.com>
2024-03-11 21:24:18 +0100

    initial

---
+++ Assets/Scripts/UI/versionToText.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Analytics;
+public class version : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI versionText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        versionText.text += Application.version;
+    }
+}
