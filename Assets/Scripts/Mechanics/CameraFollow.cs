c721bd4c2362403905fe468c9be32f9cc191254d
Jaroslaw Ogonowski <dza.ogonowski@gmail.com>
2024-03-14 21:33:31 +0100

    Better camera working
    Signed-off-by: Jaroslaw Ogonowski <dza.ogonowski@gmail.com>

--- Assets/Scripts/Mechanics/CameraFollow.cs
+++ Assets/Scripts/Mechanics/CameraFollow.cs
@@ -7,11 +7,15 @@ public class CameraFollow : MonoBehaviour
     public float rotationSpeed = 5f; // Prêdkoœæ obrotu kamery
 
     private Vector3 offset; // Przechowuje pocz¹tkowy wektor odleg³oœci miêdzy kamer¹ a obiektem
+    private float originalRotationX; // Pierwotna rotacja kamery w osi X
 
     void Start()
     {
         // Obliczanie pocz¹tkowego wektora odleg³oœci
         offset = transform.position - target.position;
+
+        // Zachowanie pierwotnej rotacji kamery w osi X
+        originalRotationX = transform.eulerAngles.x;
     }
 
     void LateUpdate()
@@ -22,14 +26,17 @@ public class CameraFollow : MonoBehaviour
         // Interpolacja liniowa miêdzy aktualn¹ a docelow¹ pozycj¹ kamery
         transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
 
-        // Obliczanie kierunku, w którym porusza siê postaæ
+        // Obliczanie wektora ruchu postaci
         Vector3 movementDirection = target.position - transform.position;
 
-        // Obracanie kamery w kierunku ruchu postaci
+        // Obracanie kamery w kierunku wektora ruchu postaci
         if (movementDirection != Vector3.zero)
         {
             Quaternion targetRotation = Quaternion.LookRotation(movementDirection);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            // Ustawienie pierwotnej rotacji kamery w osi X
+            transform.rotation = Quaternion.Euler(originalRotationX, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
         }
     }
 }
