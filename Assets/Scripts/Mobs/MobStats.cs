d72f860d9ed3c695f28facecd8c9b8ff4ef4ee8f
Jaroslaw Ogonowski <dza.ogonowski@gmail.com>
2024-04-20 23:44:11 +0200

    Add mobStats

---
+++ Assets/Scripts/Mobs/MobStats.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobStats : MonoBehaviour
+{
+    public string name;
+    public string rarity;
+    public string group;
+    public int hp;
+    public int maxhp;
+    public int armor;
+    public int magicArmor;
+    public int minDmg;
+    public int maxDmg;
+    public int speed;
+    public bool tameable;
+    public int tameDificult;
+    public string owner;
+    public bool legend;
+    public GameObject[] loot;
+}
