15fa6a2460c9e9f844804dc8fc065590a9529911
Jaroslaw Ogonowski <dza.ogonowski@gmail.com>
2024-04-20 22:48:55 +0200

    CameraFollow script fixed

--- Assets/Scripts/Mechanics/CameraFollow.cs
+++ Assets/Scripts/Mechanics/CameraFollow.cs
@@ -27,19 +27,6 @@ public class CameraFollow : MonoBehaviour
         transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
 
 
-
-
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-
-            if (touch.phase == TouchPhase.Moved)
-            {
-                float rotationY = -touch.deltaPosition.x * rotationSpeed * Time.deltaTime;
-                transform.RotateAround(target.position, Vector3.up, rotationY);
-            }
-            else
-            {
                 // Obliczanie docelowej rotacji kamery
                 Quaternion targetRotation = Quaternion.LookRotation(target.position - transform.position);
 
@@ -48,8 +35,8 @@ public class CameraFollow : MonoBehaviour
 
                 // Ustawienie pierwotnej rotacji kamery w osi X
                 transform.rotation = Quaternion.Euler(originalRotationX, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
-            }
+     
 
-        }
+   
     }
 }
