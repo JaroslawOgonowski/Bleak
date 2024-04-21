235c1dcfcb8425f91f9ac6a2261dbad92a5bbdd8
Jaroslaw Ogonowski <dza.ogonowski@gmail.com>
2024-04-21 22:48:24 +0200

    Add SetLayerRecursively to mini camera scripts

--- Assets/Scripts/UI/MobInfoPanelManager.cs
+++ Assets/Scripts/UI/MobInfoPanelManager.cs
@@ -8,6 +8,7 @@ public class MobInfoPanelManager : MonoBehaviour
     [SerializeField] private GameObject mobPanel;
     [SerializeField] private Button mobPanelHide;
     [SerializeField] private GameObject camera;
+    private GameObject lastCameraTarget;
     private void OnEnable()
     {
         mobPanel.SetActive(false);
@@ -15,6 +16,15 @@ public class MobInfoPanelManager : MonoBehaviour
     }
     public void OpenMobPanel(GameObject clickedGO)
     {
+        if(lastCameraTarget != null)
+        {
+            SetLayerRecursively(lastCameraTarget, "Mob");
+        }
+        lastCameraTarget = clickedGO;
+
+        SetLayerRecursively(clickedGO, "CameraTarget");
+
+
         camera.GetComponent<CameraFollow>().target = clickedGO.GetComponent<Transform>();
         mobPanel.SetActive(true);
     }
@@ -22,5 +32,13 @@ public class MobInfoPanelManager : MonoBehaviour
     private void mobPanelHideOnClick()
     {
         mobPanel.SetActive(false);
+    }
+    private void SetLayerRecursively(GameObject obj, string layerName)
+    {
+        obj.layer = LayerMask.NameToLayer(layerName);
+        foreach (Transform child in obj.transform)
+        {
+            SetLayerRecursively(child.gameObject, layerName);
+        }
     }
 }
