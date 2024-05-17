e293d92c69cf705b2da16bf568e3a1fc5b619a11
Jaroslaw Ogonowski <dza.ogonowski@gmail.com>
2024-05-17 23:57:42 +0200

    Gathering move added

--- Assets/Plugins/SimpleInput/Example/ExamplePlayerController.cs
+++ Assets/Plugins/SimpleInput/Example/ExamplePlayerController.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Collections;
 public class ExamplePlayerController : MonoBehaviour
 {
+    public static ExamplePlayerController Instance;
     public Color materialColor;
     private Rigidbody m_rigidbody;
 
@@ -17,7 +18,7 @@ public class ExamplePlayerController : MonoBehaviour
     public float jumpForce = 1f;
     private float inputHorizontal;
     private float inputVertical;
-    private Animator animator;
+    public Animator animator;
     private bool isMoving = false;
     private BoxCollider boxCollider;
     bool closingJumpState = false;
@@ -26,11 +27,16 @@ public class ExamplePlayerController : MonoBehaviour
     private bool jumpUp;
     private bool isGathering;
     private bool isMining;
+    public bool gatheringMove = false;
 
     [SerializeField] private float forwardSpeed;
     private bool usedFallingAnimation = false;
     void Awake()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
         animator = GetComponent<Animator>();
         //GetComponent<Renderer>().material.color = materialColor;
         m_rigidbody = GetComponent<Rigidbody>();
@@ -39,15 +45,15 @@ public class ExamplePlayerController : MonoBehaviour
 
     void Update()
     {
-        if(IsGrounded() == true && closingJumpState == false)
+        if(IsGrounded() == true && closingJumpState == false && gatheringMove == false)
         {
             StartCoroutine(closeJumpAnimation());
         }
-        if (IsGrounded() == false && closingJumpState == false && animator.GetBool("Jump_up") == false && jumpUp == true)
+        if (IsGrounded() == false && closingJumpState == false && animator.GetBool("Jump_up") == false && jumpUp == true && gatheringMove == false)
         {
             animator.SetBool("Jump_up", true);
         }
-        if (IsGrounded() == false && closingJumpState == false && falling == true && usedFallingAnimation == false)
+        if (IsGrounded() == false && closingJumpState == false && falling == true && usedFallingAnimation == false && gatheringMove == false)
         {
             usedFallingAnimation = true;
             animator.SetBool("Jump_up", false);
@@ -59,24 +65,24 @@ public class ExamplePlayerController : MonoBehaviour
         transform.Rotate(0f, inputHorizontal * rotationSpeed, 0f, Space.World);
         isMoving = Mathf.Abs(inputVertical) > 0.1f;
 
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true || SimpleInput.GetButtonDown(jumpButton) && IsGrounded() == true)
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true || SimpleInput.GetButtonDown(jumpButton) && IsGrounded() == true && gatheringMove == false)
         {
             m_rigidbody.AddForce(0f, jumpForce, 0f, ForceMode.Impulse);
         }
 
-        if (Input.GetKeyDown(KeyCode.W) && IsGrounded() == true)
+        if (Input.GetKeyDown(KeyCode.W) && IsGrounded() == true && gatheringMove == false)
         {
             m_rigidbody.AddForce(transform.forward * 3f, ForceMode.Impulse);
         }
-        if (Input.GetKeyDown(KeyCode.S) && IsGrounded() == true)
+        if (Input.GetKeyDown(KeyCode.S) && IsGrounded() == true && gatheringMove == false)
         {
             m_rigidbody.AddForce(-transform.forward * 3f, ForceMode.Impulse);
         }
-        if (Input.GetKeyDown(KeyCode.A) && IsGrounded() == true)
+        if (Input.GetKeyDown(KeyCode.A) && IsGrounded() == true && gatheringMove == false)
         {
             transform.Rotate(0f, -8f, 0f);
         }
-        if (Input.GetKeyDown(KeyCode.D) && IsGrounded() == true)
+        if (Input.GetKeyDown(KeyCode.D) && IsGrounded() == true && gatheringMove == false)
         {
             transform.Rotate(0f, 8f, 0f);
         }
@@ -99,18 +105,18 @@ public class ExamplePlayerController : MonoBehaviour
 
         forwardSpeed = Vector3.Dot(movement.normalized, transform.forward);
 
-        if (forwardSpeed > 0.1f && IsGrounded() == true)
+        if (forwardSpeed > 0.1f && IsGrounded() == true && gatheringMove == false)
         {
             animator.SetBool("RunForward", true);
             animator.SetBool("RunBackward", false);
         }
-        else if (forwardSpeed < -0.1f && IsGrounded() == true)
+        else if (forwardSpeed < -0.1f && IsGrounded() == true && gatheringMove == false)
         {   
             animator.SetBool("RunForward", false);
             animator.SetBool("RunBackward", true);
         }
-        else
-        {
+        else if (gatheringMove == false) 
+        { 
             animator.SetBool("RunForward", false);
             animator.SetBool("RunBackward", false);
         }
--- Assets/Scripts/Mechanics/Mining.cs
+++ Assets/Scripts/Mechanics/Mining.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Mining : MonoBehaviour
-{
+{   
     [SerializeField] private GameObject character;
     [SerializeField] private GameObject miningTarget;
     [SerializeField] private Button miningButton;
@@ -25,24 +25,23 @@ public class Mining : MonoBehaviour
 
     IEnumerator MoveCharacterToTarget()
     {
-        Animator animator = character.GetComponent<Animator>();
-
         while (true)
         {
-
+            ExamplePlayerController.Instance.gatheringMove = true;
             Vector3 direction = miningTarget.transform.position - character.transform.position;
             float currentDistance = direction.magnitude;
-
+           
             // Check if character is too close or too far
             if (currentDistance > distance + 0.1f)
             {
+                ExamplePlayerController.Instance.animator.SetBool("RunForward", true);
                 // Move closer
                 direction.Normalize();
                 character.transform.Translate(direction * moveSpeed * Time.deltaTime, Space.World);
             }
             else if (currentDistance < distance - 0.1f)
             {
-                animator.SetBool("RunForward", true);
+                ExamplePlayerController.Instance.animator.SetBool("RunBackward", true);
                 // Move away
                 direction.Normalize();
                 character.transform.Translate(-direction * moveSpeed * Time.deltaTime, Space.World);
@@ -51,6 +50,10 @@ public class Mining : MonoBehaviour
             {
                 // Character is at the correct distance
                 Debug.Log("Character is at the correct distance from the mining target.");
+                ExamplePlayerController.Instance.animator.SetBool("RunBackward", false);
+                ExamplePlayerController.Instance.animator.SetBool("RunForward", false);
+                ExamplePlayerController.Instance.gatheringMove = false;
+                ExamplePlayerController.Instance.animator.SetBool("Mining", true);
                 yield break;
             }
 
