6238feb1b02d9f874b045d199b9df01ae5b3325b
Jaroslaw Ogonowski <dza.ogonowski@gmail.com>
2024-07-13 22:14:52 +0200

    end iteraction trigger

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -13,6 +13,7 @@ public class Interactor : MonoBehaviour
     private void Update()
     {
         var colliders = Physics.OverlapSphere(InteractionPoint.position, InteractionPointRadius, InteractionLayer);
+        IInteractable currentInteractable = null;
 
         if (Keyboard.current.eKey.wasPressedThisFrame)
         {
@@ -23,9 +24,26 @@ public class Interactor : MonoBehaviour
                 if (interactable != null)
                 {
                     Debug.Log("Interacting with: " + colliders[i].name);
+                    currentInteractable = interactable;
                     StartInteraction(interactable);
                 }
             }
+        }
+        bool stillInRange = false;
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].GetComponent<IInteractable>() == currentInteractable)
+            {
+                stillInRange = true;
+                break;
+            }
+        }
+
+        // If the player is no longer in range of the current interactable, end the interaction
+        if (!stillInRange && currentInteractable != null)
+        {
+            EndInteraction();
+            currentInteractable = null;
         }
     }
 
