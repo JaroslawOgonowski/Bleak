1ffb6d49bfea637980b485724c7faab819066bd8
Jaroslaw Ogonowski <dza.ogonowski@gmail.com>
2024-08-08 13:14:49 +0200

    nav system

--- Assets/Scripts/Animations/ObstacleAvoidance.cs
+++ Assets/Scripts/Animations/ObstacleAvoidance.cs
@@ -1,42 +1,30 @@
+
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ObstacleAvoidance : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float detectionDistance = 2.0f;
-    public float avoidanceStrength = 1.0f;
-    public LayerMask obstacleMask;
-    public float rotationSpeed = 2.0f; // Nowa zmienna do kontrolowania szybkoœci obrotu
+    public Transform player;
+    private NavMeshAgent nav;
+    private Vector3 targetDir;
 
-    private Vector3 direction;
-
+    // Start is called before the first frame update
     void Start()
     {
-        // Inicjalny kierunek ruchu
-        direction = transform.forward;
+        nav = GetComponent<NavMeshAgent>();
     }
 
+    // Update is called once per frame
     void Update()
     {
-        // Wykonywanie raycasta przed postaci¹
-        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, detectionDistance, obstacleMask))
+        targetDir = player.position - transform.position;
+        float angle = Vector3.Angle(targetDir, transform.forward);
+
+        if (angle >= -70 && angle <= 70)
         {
-            // Obliczanie wektora unikania
-            Vector3 avoidance = Vector3.Cross(hit.normal, Vector3.up) * avoidanceStrength;
-
-            // Zmiana kierunku
-            direction += avoidance;
-            direction.Normalize();
-        }
-
-        // Aktualizacja pozycji
-        transform.position += direction * speed * Time.deltaTime;
-
-        // P³ynne obracanie w kierunku ruchu
-        if (direction != Vector3.zero)
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            nav.SetDestination(player.position);
         }
     }
 }
